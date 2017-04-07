089b519e3ed5271831042ed7408936a0c809e4e2
James Lett <jlett89@gmail.com>
2017-04-06 20:43:59 -0700

    using matrix to build tetris shapes

--- tetris.js
+++ tetris.js
@@ -1,5 +1,11 @@
 const canvas = document.getElementById('tetris')
 const context = canvas.getContext('2d')
 
-context.fillStyle = '#000'
+context.fillStyle = '#000' // Filling in canvas
 context.fillRect(0, 0, canvas.width, canvas.height);
+
+const matrix = [
+      [0, 0, 0],
+      [1, 1, 1],      // matrix to build the tetris blocks
+      [0, 1, 0],      // Thanks smarter people
+]
