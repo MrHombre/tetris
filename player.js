20fe14da6daa0e8dbcad2b638f19999d27316015
James <jlett89@gmail.com>
2017-04-27 14:47:33 -0700

    add new player js file and referencing it

---
+++ player.js
@@ -0,0 +1,11 @@
+//Player structure hopefully working for 2-player
+class Player
+{
+	construstor()
+	{
+		this.pos = { x: 0, y: 0 };
+  		this.matrix = null;
+  		this;score = 0;
+	}
+}
+
--- tetris.js
+++ tetris.js
@@ -257,12 +257,8 @@ const colors = [
 // Creating our arena
 const arena = createMatrix(12, 20);
 
-//Player structure
-const player = {
-  pos: { x: 0, y: 0 },
-  matrix: null,
-  score: 0,
-};
+//Player Structure
+const player = new Player;
 
 // Using Keycode instead of key
 document.addEventListener('keydown', event => {
