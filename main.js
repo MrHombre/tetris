7faae5c5ae7e859dea05e9a48a7e4a2f2533d07a
James <jlett89@gmail.com>
2017-04-29 15:30:44 -0700

    moved update to tetris.js create const tetris

--- main.js
+++ main.js
@@ -51,49 +51,6 @@ function createPiece(type) {
   }
 }
 
-//General draw function
-function draw() {
-  // Filling in canvas
-  context.fillStyle = '#000';
-  context.fillRect(0, 0, canvas.width, canvas.height);
-
-  drawMatrix(arena.matrix, { x: 0, y: 0 });
-  //Calling player
-  drawMatrix(player.matrix, player.pos);
-}
-
-//Wrapping maxtix in a function
-function drawMatrix(matrix, offset) {
-  // Drawing the first piece
-  matrix.forEach((row, y) => {
-    // Iterating over the row
-    row.forEach((value, x) => {
-      // Checking to make sure the value isn't 0
-      if (value !== 0) {
-        // adding colorPP
-        context.fillStyle = colors[value];
-        // x=left, y=right, 1=width, 1=height
-        context.fillRect(x + offset.x, // offset should let us move
-                         y + offset.y, // blocks later
-                         1, 1);
-      }
-    });
-  });
-}
-
-let lastTime = 0;
-/* Update Function: Continuously draws the game
-using Animation Frame */
-function update(time = 0) {
-  //formating time so it's easier for us
-  const deltaTime = time - lastTime;
-  lastTime = time;
-
-  player.update(deltaTime);
-
-  draw();
-  requestAnimationFrame(update);
-}
 
 // Score
 function updateScore() {
@@ -111,6 +68,8 @@ const colors = [
   '#FFA500',
   '#e5acb6',
 ]
+
+const tetris = new Tetris;
 
 // Creating our arena
 const arena = new Arena(12, 20);
@@ -135,4 +94,3 @@ document.addEventListener('keydown', event => {
 
 player.reset();
 updateScore();
-update();
--- tetris.js
+++ tetris.js
@@ -2,6 +2,50 @@ class Tetris
 {
 	constructor()
 	{
-	
+		let lastTime = 0;
+		/* Update Function: Continuously draws the game
+		using Animation Frame */
+		function update(time = 0) {
+		  //formating time so it's easier for us
+		  const deltaTime = time - lastTime;
+		  lastTime = time;
+
+		  player.update(deltaTime);
+
+		  this.draw();
+		  requestAnimationFrame(update);
+		}
+	}
+
+	//General draw function
+	draw() 
+	{
+	  // Filling in canvas
+	  context.fillStyle = '#000';
+	  context.fillRect(0, 0, canvas.width, canvas.height);
+
+	  this.drawMatrix(arena.matrix, { x: 0, y: 0 });
+	  //Calling player
+	  this.drawMatrix(player.matrix, player.pos);
+	}
+
+	//Wrapping maxtix in a function
+	drawMatrix(matrix, offset) 
+	{
+	  // Drawing the first piece
+	  matrix.forEach((row, y) => {
+	    // Iterating over the row
+	    row.forEach((value, x) => {
+	      // Checking to make sure the value isn't 0
+	      if (value !== 0) {
+	        // adding colorPP
+	        context.fillStyle = colors[value];
+	        // x=left, y=right, 1=width, 1=height
+	        context.fillRect(x + offset.x, // offset should let us move
+	                         y + offset.y, // blocks later
+	                         1, 1);
+	      }
+	    });
+	  });
 	}
 }
